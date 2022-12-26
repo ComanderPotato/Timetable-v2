8b6dfcdc250209e7cf6fdf6c471e4e7ef1179802
Tom Golding <tomgolding2012@outlook.com>
2022-12-26 18:53:22 +1100

    Build

--- src/layouts/ListItem.js
+++ src/layouts/ListItem.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useContext, useState} from 'react'
-import getDiffDays from '../utils/getDiffDays';
+import getDiffDays from '../utils/GetDiffDays';
 import './ListItem.css'
 import chevron from '../assets/chevron.svg'
 import { AssignmentsContext, AssignmentsSetterContext } from '../context/AssignmentsContext'
